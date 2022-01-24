f971099a1aded05c31e5a3794b567723e620c6fc
gawade07 <ketangawade111@gmail.com>
2022-01-24 11:00:36 +0530

    social media marketing
    responsive

---
+++ home/script.js
@@ -0,0 +1,9 @@
+let memuBtn = document.querySelector('#menu-btn');
+let menu = document.querySelector('.header .menu');
+
+memuBtn.onclick=() =>{
+
+    menu.classList.toggle('active');
+    
+    
+}
